3fd3753fdea59b20d5c77a190be8ba09b4264b4b
Gatewaynode <44749714+gatewaynode@users.noreply.github.com>
2021-08-01 16:48:54 -0400

    Almost finished the Rust stub, it's working somewhat.

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,3 +1,22 @@
+use clap::{App, Arg};
+
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new("cli-stub")
+        .version("0.1")
+        .about("This is a just a stub of a command line app")
+        .author("gatewaynode")
+        .arg(
+            Arg::with_name("verbose")
+                .short("v")
+                .long("verbose")
+                .help("Output more information")
+                .takes_value(false),
+        )
+        .get_matches();
+    let mut verbose = false;
+
+    if matches.is_present("verbose") {
+        verbose = true;
+    }
+    println!("You entered {}", verbose);
 }
